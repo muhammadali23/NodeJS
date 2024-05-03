6e3dae65b4fe7b4f265a7d02fed3ccb484e4ed2f
muhammadali23 <muhammadali121999@gmail.com>
2024-05-03 15:10:05 +0500

    http_method

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const server = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}, ${req.url} : new request recevied\n`;
+  const log = `${Date.now()},${req.method}, ${
+    req.url
+  } : new request recevied\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
-        res.end("homepage");
+        if (req.method === "GET") res.end("homepage");
         break;
       case "/about":
-        res.end("I am muhammad ali");
+        const userName = myUrl.query.myName;
+        res.end(`hi, ${userName}`);
         break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`here is results for ${search}`);
+      case "/signup":
+        if (req.method === "GET") {
+          res.end("This is a signup form");
+        } else if (req.method === "POST") {
+          //DB query
+          res.end("Success");
+        }
       default:
         res.end("404 Page not found");
     }
